10d7ed38ba0ada0923a28f77e87215b2a93b5c76
Abdelhakim Nassif <anassif@e2r13p9.1337.ma>
2021-10-23 16:30:32 +0100

    construct

--- vector/Vector.hpp
+++ vector/Vector.hpp
@@ -13,27 +13,42 @@ namespace ft
             typedef Alloc allocator_type;
             typedef T value_type;
 
-
+            /* constructors  */
             explicit vector (const allocator_type& alloc = allocator_type()): al(alloc), arr(NULL),size(0),capacity(0)
             {
                 
             }
-            explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
+            explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()): al(alloc)
             {
-                al.allocate(n);
+                //need to use construct so it doesnt use = op
+                arr = al.allocate(n);
                 for (int i = 0; i < n; i++)
-                    arr[i] = val;
-                size = n, capacity = n;
+                    al.construct(arr[i], val);
+                size = n; capacity = n;
             }
-            template <class InputIterator> vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
+            template <class InputIterator> vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()):al(alloc)
             {
-                
+                arr = allocate(last - first);
+                capacity = last - first;
+                size = capacity;
+                int i = 0;
+                while(last != first)
+                {
+                    al.construct(arr[i], *first);
+                    first++;
+                    i++;
+                }
             }
             vector (const vector& x)
             {
-
+                *this = x;
             }
+            /*end contructors*/
 
+            Vector &operator= (const vector& x)
+            {
+                
+            }
         private:
             allocator_type al;
             value_type *arr;
