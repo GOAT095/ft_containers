ca7dbb339882c11d78b143fc3f492b7d3df27cda
GOAT095 <hakimoxxx@gmail.com>
2021-10-24 15:20:09 +0100

    added = & destructor

--- vector/Vector.hpp
+++ vector/Vector.hpp
@@ -2,16 +2,19 @@
 
 #include <iostream>
 #include <memory>
-
+#include <STL>
+#include 
 namespace ft
 {
     
-    template < class T, class Alloc = allocator<T>
+    template < class T, class Alloc = std::allocator<T> >
     class Vector
     {
         public:
             typedef Alloc allocator_type;
             typedef T value_type;
+            typedef size_t size_type;
+            typedef iterator std::random_access_iterator_tag<T>;
 
             /* constructors  */
             explicit vector (const allocator_type& alloc = allocator_type()): al(alloc), arr(NULL),size(0),capacity(0)
@@ -22,7 +25,7 @@ namespace ft
             {
                 //need to use construct so it doesnt use = op
                 arr = al.allocate(n);
-                for (int i = 0; i < n; i++)
+                for (size_type i = 0; i < n; i++)
                     al.construct(arr[i], val);
                 size = n; capacity = n;
             }
@@ -31,7 +34,7 @@ namespace ft
                 arr = allocate(last - first);
                 capacity = last - first;
                 size = capacity;
-                int i = 0;
+                size_type i = 0;
                 while(last != first)
                 {
                     al.construct(arr[i], *first);
@@ -41,19 +44,38 @@ namespace ft
             }
             vector (const vector& x)
             {
+                arr = nullptr;
+                size = 0;
+                capacity = 0;
                 *this = x;
             }
             /*end contructors*/
 
             Vector &operator= (const vector& x)
             {
-                
+                //from copy constructor
+                if (capacity != 0)
+                {
+                    for(size_type i = 0; i < size; i++)
+                        al.destroy(*arr[i]);
+                }
+                //normal behavior
+                for(size_type i = 0; i < x.size; i++)
+                    al.construct(arr[i], x.arr[i]);
+                size = x.size;
+                capacity = x.capacity;
+            }
+            ~Vector(void)
+            {
+                for(size_type i = 0; i < size; ++i)
+			        al.destroy(&arr[i]);
+		        al.deallocate(arr, capacity);
             }
         private:
             allocator_type al;
             value_type *arr;
-            size_t size;
-            size_t capacity;
+            size_type size;
+            size_type capacity;
 
-    }
-}
+    };
+};
