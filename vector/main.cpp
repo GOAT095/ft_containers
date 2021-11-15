d4511910ca5d4e3b9f80265da6b3f5f376f1fa8d
Abdelhakim Nassif <anassif@e2r13p12.1337.ma>
2021-11-15 18:35:24 +0100

    is intergral in

--- vector/main.cpp
+++ vector/main.cpp
@@ -4,23 +4,29 @@
 int main (void)
 {
     // int i = 0;
-    ft::Vector<int> myvector(5);
-    //for(int i = 0; i < 5; i++)
-        myvector.push_back(8);
-    ft::Vector<int>::iterator it =  myvector.begin();
-    // for(int i = 0; i < 5; i++)
-    // {   std::cout << *it; it++;}
-    // ft::Vector<int>::iterator it2 =  myvector.end();
-    // std::cout << *(--it2) << std::endl;
+    // ft::Vector<int> myvector(5);
+    // //for(int i = 0; i < 5; i++)
+    //     myvector.push_back(8);
+    // ft::Vector<int>::iterator it =  myvector.begin();
+    // // for(int i = 0; i < 5; i++)
+    // // {   std::cout << *it; it++;}
+    // // ft::Vector<int>::iterator it2 =  myvector.end();
+    // // std::cout << *(--it2) << std::endl;
 
-    // working fine as expected !
+    // // working fine as expected !
 
-    std::cout << myvector.size() << std::endl;
-    // std::cout << myvector.max_size() << std::endl;;
-    std::cout << myvector.capacity() << std::endl;;
+    // std::cout << myvector.size() << std::endl;
+    // // std::cout << myvector.max_size() << std::endl;;
+    // std::cout << myvector.capacity() << std::endl;;
     
-    myvector.insert(it, 1111);
-    std::cout << *it << std::endl;
+    // myvector.insert(it, 1111);
+    // std::cout << *it << std::endl;
     //working fine
+
+    // std::cout << std::boolalpha;
+    // std::cout << "is_integral:" << std::endl;
+    // std::cout << "char: " << ft::is_integral<char>::value << std::endl;
+    // std::cout << "int: " << ft::is_integral<int>::value << std::endl;
+    // std::cout << "float: " << ft::is_integral<float>::value << std::endl;
     return 0;
 }
--- vector/utils.hpp
+++ vector/utils.hpp
@@ -2,20 +2,44 @@
 #include <iostream>
 
  //is_integral
-    template< class T >
-    struct is_integral
+namespace ft{
+    template< class T > struct is_integral{static const bool value = false;};
+    template<> struct is_integral<int>{static const bool value = true;};
+    template<> struct is_integral<bool>{static const bool value = true;};
+    template<> struct is_integral<char>{static const bool value = true;};
+    template<> struct is_integral<char16_t>{static const bool value = true;};
+    template<> struct is_integral<char32_t>{static const bool value = true;};
+    template<> struct is_integral<wchar_t>{static const bool value = true;};
+    template<> struct is_integral<signed char>{static const bool value = true;};
+    template<> struct is_integral<short int>{static const bool value = true;};
+    template<> struct is_integral<long int>{static const bool value = true;};
+    template<> struct is_integral<long long int>{static const bool value = true;};
+    template<> struct is_integral<unsigned char>{static const bool value = true;};
+    template<> struct is_integral<unsigned short int>{static const bool value = true;};
+    template<> struct is_integral<unsigned int>{static const bool value = true;};
+    template<> struct is_integral<unsigned long int>{static const bool value = true;};
+    template<> struct is_integral<unsigned long long int>{static const bool value = true;};
+
+    
+    template <class InputIterator1, class InputIterator2>
+    bool equal ( InputIterator1 first1, InputIterator1 last1, InputIterator2 first2 )
     {
-      bool value = false;
+      while (first1!=last1) {
+       if (!(*first1 == *first2))   // or: if (!pred(*first1,*first2)), for version 2
+         return false;
+       ++first1; ++first2;
+      }
+      return true;
+  }
+  template <class InputIterator1, class InputIterator2>
+  bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
+    {
+      while (first1!=last1)
+      {
+        if (first2==last2 || *first2<*first1) return false;
+        else if (*first1<*first2) return true;
+        ++first1; ++first2;
+      }
+      return (first2!=last2);
     }
-    
-    is_integral <int>                 {  value = true; };
-    is_integral <unsigned int>        {  value = true; };
-    is_integral <long>                {  value = true; };
-    is_integral <unsigned long>       {  value = true; };
-    is_integral <long long>           {  value = true; };
-    is_integral <unsigned long long>  {  value = true; };
-    is_integral <short>               {  value = true; };
-    is_integral <wchar_t>             {  value = true; };
-    is_integral <char>                {  value = true; };
-    is_integral <bool>                {  value = true; };
-    
+}
