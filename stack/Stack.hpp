3acd87aa7e0e47b6111147327d7f54a6cd45c44f
Abdelhakim Nassif <anassif@e2r13p9.1337.ma>
2021-11-20 02:08:36 +0100

    stack isnt working for some reason

--- stack/Stack.hpp
+++ stack/Stack.hpp
@@ -6,7 +6,7 @@
 /*   By: anassif <anassif@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 04:04:23 by anassif           #+#    #+#             */
-/*   Updated: 2021/11/19 23:01:49 by anassif          ###   ########.fr       */
+/*   Updated: 2021/11/20 00:45:25 by anassif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,24 @@ namespace ft{
     {
         public:
         
-            explicit Stack (const container_type& ctnr = container_type());
+           
 
             typedef size_t size_type;
             typedef Container container_type;
             typedef T value_type;
-
-            bool empty() const{return (_container->size() == 0);}
-            size_type size() const{return (_container->size());}
-            value_type& top(){return (_container->back());}
-            const value_type& top() const{return (_container->back());}
-            void push (const value_type& val){_container->push_back(val);}
-            void pop(){_container->pop_back();}
+            
+            explicit Stack (const container_type& ctnr = container_type());
+            
+            bool empty() const{return (_container.size() == 0);}
+            size_type size() const{return (_container.size());}
+            value_type& top(){return (_container.back());}
+            const value_type& top() const{return (_container.back());}
+            void push (const value_type& val){_container.push_back(val);}
+            void pop(){_container.pop_back();}
             
             
-        private:
-            value_type *_container;
+        protected:
+            container_type _container;
     };
     template <class T, class Container>
     bool operator== (const Stack<T,Container>& lhs, const Stack<T,Container>& rhs);
