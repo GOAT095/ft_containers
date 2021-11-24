d2ae0afb826ef86dd8b4d786084ba6b57db0d71f
Abdelhakim Nassif <anassif@e2r13p9.1337.ma>
2021-11-24 23:35:36 +0100

    dawda

--- map/test.cpp
+++ map/test.cpp
@@ -99,38 +99,6 @@ void printTree(Node* root, Trunk *prev, bool isLeft)
 int main()
 {
 
-	/*create root*/
-	// Node *root = newNode(1);
-	// /* following is the tree after above statement
-
-	// 		1
-	// 		/ \
-	// 	NULL NULL
-	// */
-
-	// root->left = newNode(2);
-	// root->right = newNode(3);
-	// /* 2 and 3 become left and right children of 1
-	// 				1
-	// 			/ \
-	// 			2	 3
-	// 		/ \	 / \
-	// 		NULL NULL NULL NULL
-	// */
-
-	// root->left->left = newNode(4);
-	// root->left->right = newNode(5);
-	// root->right->left = newNode(7);
-	// root->right->right = newNode(8);
-	/* 4 becomes left child of 2
-			1
-			/	 \
-		2	 3
-		/ \	 / \
-		4 NULL NULL NULL
-		/ \
-	NULL NULL
-	*/
 	Node *r = NULL;
 	r = insert(r, 2);
 	r = insert(r, 1);
