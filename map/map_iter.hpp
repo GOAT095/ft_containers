a22eb5932e053e8ba9ebd3bf2e0f11f4f296b5b1
Abdelhakim Nassif <anassif@e2r13p9.1337.ma>
2021-12-03 03:36:17 +0100

    need to start map iter avl seems fine

--- map/map_iter.hpp
+++ map/map_iter.hpp
@@ -4,4 +4,11 @@ template <class NODE, class T, class comp>
     class map_iter : public ft_iterator<std::bidirectional_iterator_tag, T>
     {
         
+        public :
+            typedef typename ft::iterator<std::bidirectional_iterator_tag, T>::value_type                value_type;
+            typedef typename ft::iterator<std::bidirectional_iterator_tag, T>::difference_type           difference_type;
+            typedef typename ft::iterator<std::bidirectional_iterator_tag, T>::pointer                   pointer;
+            typedef typename ft::iterator<std::bidirectional_iterator_tag, T>::reference                 reference;
+            typedef typename ft::iterator<std::bidirectional_iterator_tag, T>::iterator_category         iterator_category;
+        
     };
