3e32da54a7b07cb9d0961fc82e599fb6052d0182
Abdelhakim Nassif <anassif@e2r13p9.1337.ma>
2021-12-03 20:03:52 +0100

    map_iter

--- map/map_iter.hpp
+++ map/map_iter.hpp
@@ -1,32 +1,37 @@
 #pragma once
 #include "../vector/iterator.hpp"
-template <class NODE, class T, class comp>
-    class map_iter : public ft_iterator<std::bidirectional_iterator_tag, T>
-    {
-        private:
-            NODE *_p;
-        
-        public :
-            typedef typename ft::iterator<std::bidirectional_iterator_tag, T>::value_type                value_type;
-            typedef typename ft::iterator<std::bidirectional_iterator_tag, T>::difference_type           difference_type;
-            typedef typename ft::iterator<std::bidirectional_iterator_tag, T>::pointer                   pointer;
-            typedef typename ft::iterator<std::bidirectional_iterator_tag, T>::reference                 reference;
-            typedef typename ft::iterator<std::bidirectional_iterator_tag, T>::iterator_category         iterator_category;
-        
-        map_iter(){
-            _p = NULL;
-        }
-        map_iter(const NODE& node){
-            _p = node;
-        }
-        map_iter(const map_iter& copy)
+
+namespace ft{
+    template <class NODE, class T, class comp>
+        class map_iter : public ft::MyIterator<std::bidirectional_iterator_tag, T>
         {
-            *this = copy;
-        }
-        map_iter&   operator=(const map_iter& rhs)
-        {
-            _p = rhs._p;
-            return (*this);
-        }
-        ~map_iter(){}
-    };
+            private:
+                NODE *_p;
+            
+            public :
+                typedef typename ft::MyIterator<std::bidirectional_iterator_tag, T>::value_type                value_type;
+                typedef typename ft::MyIterator<std::bidirectional_iterator_tag, T>::difference_type           difference_type;
+                typedef typename ft::MyIterator<std::bidirectional_iterator_tag, T>::pointer                   pointer;
+                typedef typename ft::MyIterator<std::bidirectional_iterator_tag, T>::reference                 reference;
+                typedef typename ft::MyIterator<std::bidirectional_iterator_tag, T>::iterator_category         iterator_category;
+            
+            map_iter(){
+                _p = NULL;
+            }
+            map_iter(const NODE& node){
+                _p = node;
+            }
+            map_iter(const map_iter& copy)
+            {
+                *this = copy;
+            }
+            map_iter&   operator=(const map_iter& rhs)
+            {
+                _p = rhs._p;
+                return (*this);
+            }
+            ~map_iter(){}
+            pointer operator->() const{ return (_p->data);}
+            reference operator*() const{ return (*_p->data);}
+        };
+}
