73affd3fecf0d45ec9bfda7e8833d05cb5bae32c
Abdelhakim Nassif <anassif@e2r13p9.1337.ma>
2021-12-14 03:26:31 +0100

    fixed const iter problem

--- map/main.cpp
+++ map/main.cpp
@@ -3,23 +3,37 @@
 int main(){
 
   ft::Map<char,int> foo,bar;
+  foo['a']=100;
+  foo['b']=200;
+  bar['a']=10;
+  bar['z']=1000;
 
-  foo['x']=100;
-  foo['y']=200;
+  // foo ({{a,100},{b,200}}) vs bar ({a,10},{z,1000}}):
+  if (foo==bar) std::cout << "foo and bar are equal\n";
+  if (foo!=bar) std::cout << "foo and bar are not equal\n";
+  if (foo< bar) std::cout << "foo is less than bar\n";
+  if (foo> bar) std::cout << "foo is greater than bar\n";
+  if (foo<=bar) std::cout << "foo is less than or equal to bar\n";
+  if (foo>=bar) std::cout << "foo is greater than or equal to bar\n";
 
-  bar['a']=11;
-  bar['b']=22;
-  bar['c']=33;
+  // ft::Map<char,int> foo,bar;
 
-  foo.swap(bar);
+  // foo['x']=100;
+  // foo['y']=200;
 
-  std::cout << "foo contains:\n";
-  for (ft::Map<char,int>::iterator it=foo.begin(); it!=foo.end(); ++it)
-    std::cout << it->first << " => " << it->second << '\n';
+  // bar['a']=11;
+  // bar['b']=22;
+  // bar['c']=33;
 
-  std::cout << "bar contains:\n";
-  for (ft::Map<char,int>::iterator it=bar.begin(); it!=bar.end(); ++it)
-    std::cout << it->first << " => " << it->second << '\n';
+  // foo.swap(bar);
+
+  // std::cout << "foo contains:\n";
+  // for (ft::Map<char,int>::iterator it=foo.begin(); it!=foo.end(); ++it)
+  //   std::cout << it->first << " => " << it->second << '\n';
+
+  // std::cout << "bar contains:\n";
+  // for (ft::Map<char,int>::iterator it=bar.begin(); it!=bar.end(); ++it)
+  //   std::cout << it->first << " => " << it->second << '\n';
 
   // ft::Map<char,int> mymap;
 
