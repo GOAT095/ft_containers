824932630925fab1b1f00af2a65cf0ee1f9574b1
Abdelhakim Nassif <anassif@e2r13p9.1337.ma>
2021-12-20 23:10:44 +0100

    fixed upperbound

--- map/main.cpp
+++ map/main.cpp
@@ -9,27 +9,64 @@ struct classcomp {
 
 int main ()
 {
-  {
+  // {
 
-            /*------------------ ft::Maps ---------------------*/
-            std::map<int, std::string> m1;
-            ft::Map<int, std::string> ft_m1;
-            for (size_t i = 0; i < 1e6; i++)
-            {
-                m1.insert(std::make_pair(i, "string2"));
-                ft_m1.insert(ft::make_pair(i, "string2"));
-            }
+  //           /*------------------ ft::Maps ---------------------*/
+  //           std::map<int, std::string> m1;
+  //           ft::Map<int, std::string> ft_m1;
+  //           for (size_t i = 0; i < 1e6; i++)
+  //           {
+  //               m1.insert(std::make_pair(i, "string2"));
+  //               ft_m1.insert(ft::make_pair(i, "string2"));
+  //           }
 
-            m1.rbegin();
-            m1.rend();
-            /*-----------------------------------------------------*/
-            /*------------------ ft::Maps ---------------------*/
-            ft_m1.rbegin();
-            ft_m1.rend();
-            // / for (ft::Map<char,int>::iterator it=mymap.begin(); it!=mymap.end(); ++it)
-            //   pr
-            system("leaks a.out");
-  }
+  //           m1.rbegin();
+  //           m1.rend();
+  //           /*-----------------------------------------------------*/
+  //           /*------------------ ft::Maps ---------------------*/
+  //           ft_m1.rbegin();
+  //           ft_m1.rend();
+  //           // / for (ft::Map<char,int>::iterator it=mymap.begin(); it!=mymap.end(); ++it)
+  //           //   pr
+  //           system("leaks a.out");
+  // }
+    std::map<int, std::string> m1;
+        ft::Map<int, std::string> ft_m1;
+
+        for (size_t i = 0; i < 10; i++)
+        {
+            m1.insert(std::make_pair(i, "string2"));
+            ft_m1.insert(ft::make_pair(i, "string2"));
+        }
+
+        std::map<int, std::string> const m2(m1.rbegin(), m1.rend());
+        ft::Map<int, std::string> const ft_m2(ft_m1.rbegin(), ft_m1.rend());
+
+        /*-----------------------------------------------------*/
+        /*------------------ ft::Maps ---------------------*/
+        /*----------------------------------------------------*/
+        /*------------------ strings to store the results ----*/
+        std::string res, ft_res, c_res, c_ft_res;
+        /*----------------------------------------------------*/
+        for (std::map<int, std::string>::reverse_iterator it = m1.rbegin(); it != m1.rend(); ++it) // fill res from m1
+            res += it->second;
+        for (std::map<int, std::string>::const_reverse_iterator rit = m2.rbegin(); rit != m2.rend(); ++rit) // fill c_res from const m1
+            c_res += rit->second;
+
+        for (ft::Map<int, std::string>::reverse_iterator it = ft_m1.rbegin(); it != ft_m1.rend(); ++it) // fill ft_res from ft_m1
+            ft_res += it->second;
+        for (ft::Map<int, std::string>::const_reverse_iterator rit = ft_m2.rbegin(); rit != ft_m2.rend(); ++rit) // fill c_ft_res from const ft_m1
+            c_ft_res += rit->second;
+        int arr[] = {12, 82, 37, 64, 15};
+        ft::Map<int, int> end_test;
+        for(size_t i = 0; i < 5; ++i)
+            end_test.insert(ft::make_pair(arr[i], i));
+        ft::Map<int, int>::reverse_iterator rit = end_test.rend();
+        rit--;
+        for (size_t i = 0; i < 10; i++)
+        {
+            std::cout << rit->first << std::endl;
+        }
   return 0;
 }
   // std::map<int, std::string> m1;
